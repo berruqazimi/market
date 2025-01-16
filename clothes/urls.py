5d6d29b4b70c232acb1daef23528d03beafefb69
Berru <39466917+berruqazimi@users.noreply.github.com>
2025-01-16 23:17:18 +0100

    Update urls.py

--- clothes/urls.py
+++ clothes/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .views import (
     GarmentListView,
@@ -9,8 +8,6 @@ from .views import (
 )
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', GarmentListView.as_view(), name='garment_list'),
     path('publish/', PublishGarmentView.as_view(), name='publish_garment'),
     path('<int:pk>/unpublish/', UnpublishGarmentView.as_view(), name='unpublish_garment'),
